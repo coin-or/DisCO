823d86a39539e8bc8daab524cc7ced39caca0f95
Ted Ralphs <ted@lehigh.edu>
2006-10-09 18:42:19 +0000

    Adding Blis and BiCePS

---
+++ src/BlisLicense.h
@@ -0,0 +1,15 @@
+/*===========================================================================*
+ * This file is part of the BiCePS Linear Integer Solver (BLIS).             *
+ *                                                                           *
+ * BLIS is distributed under the Common Public License as part of the        *
+ * COIN-OR repository (http://www.coin-or.org).                              *
+ *                                                                           *
+ * Authors: Yan Xu, SAS Institute Inc.                                       *
+ *          Ted Ralphs, Lehigh University                                    *
+ *          Laszlo Ladanyi, IBM T.J. Watson Research Center                  *
+ *          Matthew Saltzman, Clemson University                             *
+ *                                                                           * 
+ *                                                                           *
+ * Copyright (C) 2001-2006, Lehigh University, Yan Xu, and Ted Ralphs.       *
+ * All Rights Reserved.                                                      *
+ *===========================================================================*/
