488314b210f77acbcb19c6bfaca2b189dd55b212
Yan Xu <sasyanxu@yahoo.com>
2006-11-16 01:54:40 +0000

    presolve

---
+++ Blis/src/BlisPresolve.cpp
@@ -0,0 +1,43 @@
+/*===========================================================================*
+ * This file is part of the BiCePS Linear Integer Solver (BLIS).             *
+ *                                                                           *
+ * BLIS is distributed under the Common Public License as part of the        *
+ * COIN-OR repository (http://www.coin-or.org).                              *
+ *                                                                           *
+ * Authors: Yan Xu, SAS Institute Inc.                                       *
+ *          Ted Ralphs, Lehigh University                                    *
+ *          Laszlo Ladanyi, IBM T.J. Watson Research Center                  *
+ *          Matthew Saltzman, Clemson University                             *
+ *                                                                           * 
+ *                                                                           *
+ * Copyright (C) 2001-2006, Lehigh University, Yan Xu, and Ted Ralphs.       *
+ * All Rights Reserved.                                                      *
+ *===========================================================================*/
+
+#include "BlisPresolve.h"
+
+//#############################################################################
+
+OsiSolverInterface *
+BlisPresolve::preprocess(OsiSolverInterface & origModel,
+                         double feasibilityTolerance,
+                         bool keepIntegers,
+                         int numberPasses,
+                         const char * prohibited)
+{
+    return presolvedModel(origModel,
+                          feasibilityTolerance,
+                          keepIntegers,
+                          numberPasses,
+                          prohibited);
+}
+
+//#############################################################################
+
+void 
+BlisPresolve::postprocess(bool updateStatus)
+{
+    postsolve(updateStatus);
+}
+
+//#############################################################################
---
+++ Blis/src/BlisPresolve.h
@@ -0,0 +1,55 @@
+/*===========================================================================*
+ * This file is part of the BiCePS Linear Integer Solver (BLIS).             *
+ *                                                                           *
+ * BLIS is distributed under the Common Public License as part of the        *
+ * COIN-OR repository (http://www.coin-or.org).                              *
+ *                                                                           *
+ * Authors: Yan Xu, SAS Institute Inc.                                       *
+ *          Ted Ralphs, Lehigh University                                    *
+ *          Laszlo Ladanyi, IBM T.J. Watson Research Center                  *
+ *          Matthew Saltzman, Clemson University                             *
+ *                                                                           * 
+ *                                                                           *
+ * Copyright (C) 2001-2006, Lehigh University, Yan Xu, and Ted Ralphs.       *
+ * All Rights Reserved.                                                      *
+ *===========================================================================*/
+
+#ifndef BlisPresolve_H_
+#define BlisPresolve_H_
+
+#if defined(_MSC_VER)
+// Turn off compiler warning about long names
+#  pragma warning(disable:4786)
+#endif
+
+#include "OsiPresolve.hpp"
+
+//#############################################################################
+
+/** A interface to Osi/Coin Presolve. */
+class BlisPresolve : public OsiPresolve
+{
+private:
+    
+public:
+    
+    /** Default constructor (empty object) */
+    BlisPresolve() {};
+    
+    /** Virtual destructor */
+    virtual ~BlisPresolve(){};
+
+    /** Presolve */
+    virtual OsiSolverInterface *preprocess(OsiSolverInterface & origModel,
+                                           double feasibilityTolerance=0.0,
+                                           bool keepIntegers=true,
+                                           int numberPasses=5,
+                                           const char * prohibited=NULL);
+
+    /** Postsolve */
+    virtual void postprocess(bool updateStatus=true);
+};
+
+#endif
+
+//#############################################################################
