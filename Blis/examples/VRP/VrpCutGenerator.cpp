17f3046115c7f7c85a9e507e20cbfa05951448f7
Ted Ralphs <ted@lehigh.edu>
2007-04-09 17:27:07 +0000

    Changing copyright notices

--- Blis/examples/VRP/VrpCutGenerator.cpp
+++ Blis/examples/VRP/VrpCutGenerator.cpp
@@ -1,17 +1,14 @@
-
 /*===========================================================================*
- * This file is part of the BiCePS Linear Integer Solver (BLIS).             *
+ * This file is part of a solver for the Vehicle Routing Problem             *
+ * developed using the BiCePS Linear Integer Solver (BLIS).                  *
  *                                                                           *
- * BLIS is distributed under the Common Public License as part of the        *
- * COIN-OR repository (http://www.coin-or.org).                              *
+ * This solver is distributed under the Common Public License as part of     * 
+ * the COIN-OR repository (http://www.coin-or.org).                          *
  *                                                                           *
- * Authors: Yan Xu, SAS Institute Inc.                                       *
+ * Authors: Yan Xu, Lehigh University                                        *
  *          Ted Ralphs, Lehigh University                                    *
- *          Laszlo Ladanyi, IBM T.J. Watson Research Center                  *
- *          Matthew Saltzman, Clemson University                             *
- *                                                                           * 
  *                                                                           *
- * Copyright (C) 2001-2006, Lehigh University, Yan Xu, and Ted Ralphs.       *
+ * Copyright (C) 2007 Yan Xu and Ted Ralphs.                                 *
  * All Rights Reserved.                                                      *
  *===========================================================================*/
 
