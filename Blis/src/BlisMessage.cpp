eee0c033fbf68d780d5518ac3c60837c52359350
Ted Ralphs <ted@lehigh.edu>
2007-05-20 23:01:38 +0000

    Changing version number for Bls

--- Blis/src/BlisMessage.cpp
+++ Blis/src/BlisMessage.cpp
@@ -36,7 +36,7 @@ static Blis_message us_english[]=
     {BLIS_HEUR_STAT_NODE, 65, 1, "Node %d, called %s heuristic %d times, found %d solutions, CPU time %.4f seconds, current strategy %d"},
     {BLIS_ROOT_PROCESS, 30, 1, "Processing the root node (%d rows, %d columns)"},
     {BLIS_ROOT_TIME, 35, 1, "Processing the first root relaxation took CPU time %.4f seconds"},
-    {BLIS_S_VERSION, 1, 1, "BLIS version 0.7.0"},
+    {BLIS_S_VERSION, 1, 1, "BLIS version 0.8.0"},
     {BLIS_W_LP, 20, 1, "WARNING: The Problem does not have integer variables"},
     {BLIS_DUMMY_END, 999999, 0, ""}
 };
