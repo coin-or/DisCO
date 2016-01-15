53812bca5cf286cf8ed0e9c10e91e21762af47cc
Aykut Bulut <aykutblt@gmail.com>
2016-01-15 08:57:23 -0500

    compile script improved.

--- compile.sh
+++ compile.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-mkdir opt_build
-cd opt_build
+mkdir -p build
+cd build
 build_dir=$PWD
 inc_dir=${build_dir%%/}/include
 lib_dir=${build_dir%%/}/lib
 pkg_dir=${lib_dir%%/}/pkgconfig
 PKG_CONFIG_PATH=${pkg_dir}:$PKG_CONFIG_PATH
-export CXXFLAGS="-std=c++11 -O3"
+export CXXFLAGS="-std=c++11 -O2"
 # configure and install Blas
 mkdir -p ThirdParty/Blas
 cd ThirdParty/Blas
@@ -14,115 +14,115 @@ cd ThirdParty/Blas
 make -j 10 install
 cd ..
 # configure and install Lapack
-mkdir Lapack
+mkdir -p Lapack
 cd Lapack
 ../../../ThirdParty/Lapack/configure --prefix=$build_dir
 make -j 10 install
 cd ..
 # configure and install ASL
-mkdir ASL
+mkdir -p ASL
 cd ASL
 ../../../ThirdParty/ASL/configure --prefix=$build_dir
 make -j 10 install
 cd ..
 # configure and install HSL
-mkdir HSL
+mkdir -p HSL
 cd HSL
 ../../../ThirdParty/HSL/configure --prefix=$build_dir
 make -j 10 install
 cd ..
 # configure and install Metis
-mkdir Metis
+mkdir -p Metis
 cd Metis
 ../../../ThirdParty/Metis/configure --prefix=$build_dir
 make -j 10 install
 cd ..
 # configure and install Mumps
-mkdir Mumps
+mkdir -p Mumps
 cd Mumps
 ../../../ThirdParty/Mumps/configure --prefix=$build_dir
 make -j 10 install
 cd ../..
 # configure and install CoinUtils
-mkdir CoinUtils
+mkdir -p CoinUtils
 cd CoinUtils
 ../../CoinUtils/configure --prefix=$build_dir
 make -j 10 install
 cd ..
 # configure and install Osi
-mkdir Osi
+mkdir -p Osi
 cd Osi
 ../../Osi/configure --prefix=$build_dir --with-mosek-incdir=/usr/local/mosek/7.1/tools/platform/linux64x86/h --with-mosek-lib="-L/usr/local/mosek/7.1/tools/platform/linux64x86/bin -lmosek64 -lmosekxx7_1 -lmosekjava7_1 -lmosekscopt7_1 -liomp5" --with-cplex-incdir=/usr/local/cplex/include/ilcplex --with-cplex-lib="-L/usr/local/cplex/lib/x86-64_linux/static_pic/ -lcplex -lm -lpthread"
 make -j 10 install
 cd ..
 # configure and install Clp
-mkdir Clp
+mkdir -p Clp
 cd Clp
 ../../Clp/configure --prefix=$build_dir
 make -j 10 install
 cd ..
 # configure and install Cgl
-mkdir Cgl
+mkdir -p Cgl
 cd Cgl
 ../../Cgl/configure --prefix=$build_dir
 make -j 10 install
 cd ..
 # configure and install Alps
-mkdir Alps
+mkdir -p Alps
 cd Alps
 ../../Alps/configure --prefix=$build_dir
 make -j 10 install
 cd ..
 # configure and install Bcps
-mkdir Bcps
+mkdir -p Bcps
 cd Bcps
 ../../Bcps/configure --prefix=$build_dir
 make -j 10 install
 cd ..
 # configure and install Osi-Conic
-mkdir OsiConic
+mkdir -p OsiConic
 cd OsiConic
 ../../OsiConic/configure --prefix=$build_dir
 make -j 10 install
 cd ..
 # configure and install Osi-Mosek
-mkdir OsiMosek
+mkdir -p OsiMosek
 cd OsiMosek
 ../../OsiMosek/configure --prefix=$build_dir
 make -j 10 install
 cd ..
 # configure and install Osi-Cplex
-mkdir OsiCplex
+mkdir -p OsiCplex
 cd OsiCplex
 ../../OsiCplex/configure --prefix=$build_dir
 make -j 10 install
 cd ..
 # configure and install ipopt
-mkdir Ipopt
+mkdir -p Ipopt
 cd Ipopt
 ../../Ipopt/configure --prefix=$build_dir
 make -j 10 install
 cd ..
 # configure and install OsiIpopt
-mkdir OsiIpopt
+mkdir -p OsiIpopt
 cd OsiIpopt
 ../../OsiIpopt/configure --prefix=$build_dir
 make -j 10 install
 cd ..
 # configure and install Cola
-mkdir Cola
+mkdir -p Cola
 cd Cola
 ../../Cola/configure --prefix=$build_dir
 make -j 10 install
 cd ..
 # configure and install CglConic
-mkdir CglConic
+mkdir -p CglConic
 cd CglConic
 ../../CglConic/configure --prefix=$build_dir
 make -j 10 install
 cd ..
 # configure and compile disco
-mkdir DisCO
+mkdir -p DisCO
 cd DisCO
-../../configure --prefix=$build_dir
+../../configure --prefix=$build_dir --with-soco-solver=oa
 make -j 10 install
