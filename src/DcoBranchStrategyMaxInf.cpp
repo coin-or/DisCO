48976e5a6ee36b2c2c4ce20e199d429716dc5140
Aykut Bulut <aykutblt@gmail.com>
2016-05-20 13:05:02 -0400

    updated for branching design changes.

--- src/DcoBranchStrategyMaxInf.cpp
+++ src/DcoBranchStrategyMaxInf.cpp
@@ -1,111 +1,65 @@
 #include "DcoBranchStrategyMaxInf.hpp"
 #include "DcoModel.hpp"
+#include "DcoTreeNode.hpp"
+#include "DcoBranchObject.hpp"
 
-DcoBranchStrategyMaxInf::DcoBranchStrategyMaxInf() {
+DcoBranchStrategyMaxInf::DcoBranchStrategyMaxInf(DcoModel * model)
+  : BcpsBranchStrategy(model) {
+  setType(DcoBranchingStrategyMaxInfeasibility);
 }
 
-DcoBranchStrategyMaxInf::DcoBranchStrategyMaxInf(DcoModel * model):
-  BcpsBranchStrategy(model) {
-}
-
-DcoBranchStrategyMaxInf::~DcoBranchStrategyMaxInf() {
-}
-
-BcpsBranchStrategy * DcoBranchStrategyMaxInf::clone() const {
-  DcoModel * model = dynamic_cast<DcoModel*>(model_);
-  return new DcoBranchStrategyMaxInf(model);
-}
-
-int DcoBranchStrategyMaxInf::createCandBranchObjects(int numPassesLeft,
-                                                     double ub) {
+int DcoBranchStrategyMaxInf::createCandBranchObjects(BcpsTreeNode * node) {
+  // get node
+  DcoTreeNode * dco_node = dynamic_cast<DcoTreeNode*>(node);
   // get model
-  DcoModel * model = dynamic_cast<DcoModel*>(model_);
-  // get integer tolerance parameter
-  double tolerance = model->dcoPar()->entry(DcoParams::integerTol);
+  DcoModel * dco_model = dynamic_cast<DcoModel*>(model());
   // get number of relaxed columns
   // we assume all relaxed columns are integer variables.
-  int num_relaxed = model->numRelaxedCols();
+  int num_relaxed = dco_model->numRelaxedCols();
   // get indices of relaxed object
-  int const * relaxed = model->relaxedCols();
+  int const * relaxed = dco_model->relaxedCols();
   // store branch objects in bobjects
   std::vector<BcpsBranchObject*> bobjects;
   // iterate over relaxed columns and populate bobjects
   for (int i=0; i<num_relaxed; ++i) {
     int preferredDir;
-    BcpsObject * curr_object = model->getVariables()[relaxed[i]];
-    double infeasibility = curr_object->infeasibility(model, preferredDir);
+    BcpsObject * curr_object = dco_model->getVariables()[relaxed[i]];
+    double infeasibility = curr_object->infeasibility(dco_model, preferredDir);
     // check the amount of infeasibility
     if (infeasibility != 0.0) {
       // create a branch object for this
       BcpsBranchObject * cb =
-        curr_object->createBranchObject(model, preferredDir);
+        curr_object->createBranchObject(dco_model, preferredDir);
+      // set score
+      cb->setScore(infeasibility);
       bobjects.push_back(cb);
     }
   }
-  // set number of branch objects
-  //setNumBranchObjects(bobjects.size());
   // add branch objects to branchObjects_
   setBranchObjects(bobjects);
   // bobjects are now owned by BcpsBranchStrategy, do not free them.
   std::vector<BcpsBranchObject*>::iterator it;
-  for (it=bobjects.begin(); it!=bobjects.end(); ++it) {
-    *it = NULL;
-  }
   bobjects.clear();
+  // set the branch object member of the node
+  dco_node->setBranchObject(new DcoBranchObject(bestBranchObject()));
   // compare branch objects and keep the best one at bestBranchObject_
   return 0;
 }
 
-/** Compare branching object thisOne to bestSoFar. If thisOne is better than
-    bestObject, return branching direction(1 or -1), otherwise return 0.  If
-    bestSorFar is NULL, then always return branching direction(1 or -1).
-
-    todo(aykut): this does not make sense to me. I think the return value
-    can be made to represent something simpler. Now it represents a direction
-    or update of the best so far.
-*/
-int DcoBranchStrategyMaxInf::betterBranchObject(BcpsBranchObject * b,
-                                                BcpsBranchObject * bestSoFar) {
+/// Compare current to other, return 1 if current is better, 0 otherwise
+int
+DcoBranchStrategyMaxInf::betterBranchObject(BcpsBranchObject const * current,
+                                            BcpsBranchObject const * other) {
   // get model
-  DcoModel * model = dynamic_cast<DcoModel*>(model_);
-  CoinMessageHandler * message_handler = model->dcoMessageHandler_;
-  CoinMessages * messages = model->dcoMessages_;
-  if (bestSoFar==NULL) {
-    // best is NULL, set it to b.
-    bestSoFar = b;
-    return b->getDirection();
+  DcoModel * dco_model = dynamic_cast<DcoModel*>(model());
+  CoinMessageHandler * message_handler = dco_model->dcoMessageHandler_;
+  CoinMessages * messages = dco_model->dcoMessages_;
+  int res;
+  if (current->score()>other->score()) {
+    res = 1;
   }
   else {
-    // compare b with bestSoFar
-    if (b==NULL) {
-      message_handler->message(9998, "Dco", "Can not compare, "
-                               "NULL branch object is given. ",
-                               'E', 0)
-        << CoinMessageEol;
-    }
-    else {
-      double value = b->getValue();
-      // distance to upper
-      double dt_upper = ceil(value) - value;
-      // distance to lower
-      double dt_lower = value - floor(value);
-      // fractional distance value
-      double d = (dt_upper<dt_lower) ? dt_upper : dt_lower;
-      // branched value for the best
-      double best_value = bestSoFar->getValue();
-      double best_dt_upper = ceil(best_value) - best_value;
-      double best_dt_lower = best_value - floor(best_value);
-      double best_d = (best_dt_upper<best_dt_lower) ?
-        best_dt_upper : best_dt_lower;
-      if (d>best_d) {
-        // update best
-        bestSoFar = b;
-        return b->getDirection();
-      }
-      else {
-        // do not update best
-        return 0;
-      }
-    }
+    res = 0;
   }
+  return res;
 }
--- src/DcoBranchStrategyMaxInf.hpp
+++ src/DcoBranchStrategyMaxInf.hpp
@@ -10,32 +10,88 @@ class DcoModel;
 
    When a node is picked from the pool and it is pregnant Alps calls
    AlpsSearchStrategy::createNewNodes(). AlpsSearchStrategy::createNewNodes()
-   calls AlpsNode::branch() and Alps process output of this function further.
+   calls AlpsNode::branch() and Alps processes output of this function further.
 
-   This function
-   Call DcoBranchStrategy[Name]::createCandBranchObjects() for the
-   corresponding strategy. This will populate branchObjects_ inherited from
-   BcpsBranchStrategy (where does bestBranchObject_ get into play?).
+   When DcoTreeNode::process() decides to branch it calls
+   DcoBranchStrategy[Name]::createCandBranchObjects(). This will populate
+   branchObjects_ inherited from BcpsBranchStrategy.
 
-   Then we create child nodes using the bestBranchObject_ in
-   DcoTreeNode::branch(). return them, Alps will add them into the node pool.
+   When DcoTreeNode::branch() is called we create child nodes using the
+   bestBranchObject_.  DcoTreeNode::branch() returns the new children nodes.
+   Alps will add them into the node pool.
 
-   Where does DcoVariable::createBranchObject get into play?
+   DcoTreeNode::process calls DcoTreeNode::boundingLoop.
+   DcoTreeNode::boundingLoop calls DcoTreeNode::branchConstrainOrPrice.
+   DcoTreeNode::branchConstrainOrPrice may decide to branch. When it does
+   DcoTreeNode::boundingLoop calls DcoBranchStrategy::createCandBranchObjects.
+   Then sets DcoTreeNode::branchObject_ to the best branch object found by
+   calling DcoBranchStrategy::bestBranchObject().
 
-   This class represents maximum infeasibility branching strategy. Inherits
-   BcpsBranchStrategy.
+   Branching is triggered by an infeasible object. An infeasible object should
+   know how to branch itself. Thus, they have member function
+   createBranchObject(). This means for each infeasible object we will have a
+   branch object.
 
-   BcpsBranchStrategy:
+   A Branch object keeps track of how many branches are created. Has detailed
+   information on creating these branches.
+
+
+
+   # Ideas/Questions
+
+   Q1: I do not see clearly what a branch object represents. For me each branch is
+   a node. I do not see how we need branch object class. Branch itself does not
+   stand for something, branching is a process that outputs something (nodes).
+
+   A1: You are right. Branching is a process and it creates new
+   nodes. Ideally the branching object data should be stored in the node class.
+   In the current implementatition the data needed for branching is separately
+   encapsulated in a branch object and it is a member of the DcoTreeNode class.
+
+   Branch strategy object (branchStrategy_) is a member of DcoModel. Once we
+   decide to branch when processing a node, we create candidates using the
+   strategy object (branchStrategy_->createCandBranchObjects()). But this
+   function has no idea what node is being branched. This creates problems
+   since branching process might need node data, (ie. pseudocost branching
+   needs the quality of the node being processed and its parent's quality).
+
+   # How to update scoring
+
+   Keeping scores for most infeasible branching strategy is easy. Each time
+   we will decide the branching variable, we will check the fractionality of
+   all variables. Things get mroe complicated for other strategies.
+
+   ## Pseudocost
+
+   When pseudocost strategy is used, we need to update statistics related to
+   performance of variables. When a branch decision is being made data only
+   from previous decisions are available. The performance of current branching
+   decison will be available only after the children's subproblems are solved.
+
+   When the statistics should be updated???? Ideally it should be updated
+   after we solve a subproblem, DcoTreeNode::bound() call.
+
+   When a branch() is called in a node, first update its score. This way we
+   update scores for each node only once.
+
+   # BcpsBranchStrategy
+
    BcpsBranchStrategy is an abstract base class for defining a branch stragey.
    It provides an interface for creating a set of branching candidates and
    comparing them.
 
-   DcoBranchStrategyMaxInf:
+   # Maximum infeasibility branching, DcoBranchStrategyMaxInf
+
+   This class represents maximum infeasibility branching strategy. Inherits
+   BcpsBranchStrategy.
+
    Implements maximum infeasibility branching in interface inherited from
    BcpsBranchStrategy.
 
-   What is a branch object?
-   Keeps necessary information for a branching to be perfomed.
+   # What is a branch object?
+
+   A branching object keeps necessary information for a branching to be
+   perfomed.
 
    BcpsBranchObject contains the member data required when choosing
    branching entities and excuting actual branching. It also has
@@ -52,18 +108,14 @@ class DcoModel;
  */
 class DcoBranchStrategyMaxInf: public BcpsBranchStrategy {
 public:
-  DcoBranchStrategyMaxInf();
   DcoBranchStrategyMaxInf(DcoModel * model);
-  virtual ~DcoBranchStrategyMaxInf();
-  virtual BcpsBranchStrategy * clone() const;
-  virtual int createCandBranchObjects(int numPassesLeft, double ub);
-  /** Compare branching object thisOne to bestSoFar. If thisOne is better than
-      bestObject, return branching direction(1 or -1), otherwise return 0.  If
-      bestSorFar is NULL, then always return branching direction(1 or -1).
-  */
-  virtual int betterBranchObject(BcpsBranchObject * b,
-                                 BcpsBranchObject * bestSoFar);
+  virtual ~DcoBranchStrategyMaxInf() {}
+  virtual int createCandBranchObjects(BcpsTreeNode * node);
+  /// Compare current to other, return 1 if current is better, 0 otherwise
+  virtual int betterBranchObject(BcpsBranchObject const * current,
+                                 BcpsBranchObject const * other);
 private:
+  DcoBranchStrategyMaxInf();
   DcoBranchStrategyMaxInf & operator=(DcoBranchStrategyMaxInf const & rhs);
   DcoBranchStrategyMaxInf(DcoBranchStrategyMaxInf const & other);
 };
