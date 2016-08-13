3d4ced1d7ea62ddd49d2142b5564a8b5d9e6a471
Aykut Bulut <aykutblt@gmail.com>
2016-08-13 17:46:00 -0400

    If else macto chain improved in main file.

--- src/DcoMain.cpp
+++ src/DcoMain.cpp
@@ -26,31 +26,24 @@ int main(int argc, char *argv[]) {
   // }
 
 #if defined(__OA__)
-  // todo(aykut) what about setting solver log level
   OsiSolverInterface * solver = new OsiClpSolverInterface();
   // for unboundedness directions set option
   dynamic_cast<OsiClpSolverInterface*>(solver)->getModelPtr()->setMoreSpecialOptions(0);
   solver->setHintParam(OsiDoInBranchAndCut, true, OsiHintDo, NULL);
   dynamic_cast<OsiClpSolverInterface*>(solver)->getModelPtr()->setLogLevel(0);
-#else
-#if defined(__OSI_MOSEK__)
+#elif defined(__OSI_MOSEK__)
   OsiConicSolverInterface * solver = new OsiMosekSolverInterface();
-#else
-#if defined(__OSI_CPLEX__)
+#elif defined(__OSI_CPLEX__)
   OsiConicSolverInterface * solver = new OsiCplexSolverInterface();
-#else
-#if defined(__COLA__)
+#elif defined(__COLA__)
   OsiConicSolverInterface * solver = new ColaModel();
 #endif
-#endif
-#endif
-#endif
+
   // Create DisCO model
   DcoModel model;
   model.setSolver(solver);
 #ifdef  COIN_HAS_MPI
   AlpsKnowledgeBrokerMPI broker(argc, argv, model);
-
 #else
   AlpsKnowledgeBrokerSerial broker(argc, argv, model);
 #endif
